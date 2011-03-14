1258adb953022ac63bdf28e599226a13b7d5261a
kleetus <kleetus@kleetus-Lenovo-G550.(none)>
2011-03-14 19:07:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimroseRfid::Application.config.secret_token = '6ea427fd9b73d52e085b149afd179029250628e8189d2b73670cf57f6141edebcbbc7ce47c8bae23e84007debae878bf3790f73c4a02bcca090beedd119961e8'
