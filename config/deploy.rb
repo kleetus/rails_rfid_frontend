a191f14cf22b0efac60d8842147af52cffcd8287
Chris Kleeschulte <ckleeschulte@primedia.com>
2011-06-29 08:24:53 -0400

    changed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,12 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.2'
+set :rvm_ruby_string, '1.9.2@rails_rfid'
 set :rvm_type, :user
 
 require 'bundler/capistrano'
 
 set :application, "rails_rfid_frontend"
 set :repository,  "git@github.com:kleetus/rails_rfid_frontend.git"
-set :use_sudo, false
 
 
 set :scm, :git
@@ -15,7 +14,7 @@ set :port, 12
 set :user, "kleetus"
 set :keep_releases, 2
 
-set :deploy_to, "/home/kleetus/RAILS_APPS"
+set :deploy_to, "/home/kleetus/RAILS_APPS/rails_rfid_frontend"
 
 role :web, "kleetus-xen.dyndns.org"
 role :app, "kleetus-xen.dyndns.org"
@@ -23,18 +22,18 @@ role :db,  "kleetus-xen.dyndns.org", :primary => true
 
 
 namespace :deploy do
-  desc "deploy restart"
+  # desc "deploy restart"
   task :restart, :roles => :app, :except => {:no_release => true} do
-    stop
-    start
+    # stop
+    # start
   end
-
-  task :start, :roles => :app, :except => {:no_release => true} do
-    run "cd #{current_path} && bundle exec unicorn_exec start"
-  end
-
-  task :stop, :roles => :app, :except => {:no_release => true} do
-    run "#{current_path}/bundle exec unicorn_exec stop"
-  end
+  # 
+  # task :start, :roles => :app, :except => {:no_release => true} do
+  #   run "cd #{current_path} && bundle exec unicorn_exec start"
+  # end
+  # 
+  # task :stop, :roles => :app, :except => {:no_release => true} do
+  #   run "#{current_path}/bundle exec unicorn_exec stop"
+  # end
 end
 
